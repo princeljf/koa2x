b65e77e30c9a8998eadd973808de399f3b4e5e10
lhhformac <384234884@qq.com>
2020-05-06 14:52:21 +0800

    koa2x

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # koa2x
 service koa2x
+view-koa/
+|
++- controllers/ <-- Controller
+|
++- node_modules/ <-- npm安装的所有依赖包
+|
++- static/
+   |
+   +- css/ <- 存放bootstrap.css等
+   |
+   +- fonts/ <- 存放字体文件
+   |
+   +- js/ <- 存放bootstrap.js等  
+|
++- views/ <-- html模板文件
+|
++- app.js <-- 使用koa的js
+|
++- controller.js <-- 扫描注册Controller
+|
++- package.json <-- 项目描述文件
+|
++- static-files.json <-- 处理静态文件
+|
++- templating.json <-- 给ctx加上render()来使用Nunjucks
+
+ 
---
+++ app.js
@@ -0,0 +1,41 @@
+const Koa = require('koa');
+const app = new Koa();
+const isProduction = process.env.NODE_ENV === 'production';
+console.log('isProduction = ', isProduction);
+const bodyParser = require('koa-bodyparser');
+const templating = require('./templating');
+const controller = require('./controller');
+
+//第一个middleware是记录URL以及页面执行时间：
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
+    await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
+});
+
+//第二个middleware处理静态文件：
+if (!isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
+
+//第三个middleware解析POST请求：
+app.use(bodyParser());
+
+//第四个middleware负责给ctx加上render()来使用Nunjucks：
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
+
+
+//最后一个middleware处理URL路由：
+app.use(controller());
+
+
+app.listen(3000);
+console.log('app started at port 3000...');
